6fa7516b38fc414da38dd6e3ec5a69126070afb8
williamrolando88 <william.rolando88@hotmail.com>
2022-06-06 20:02:33 -0500

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :brands
-  resources :lines
+  resources :lines do
+    resources :brands do
+      resources :products
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
