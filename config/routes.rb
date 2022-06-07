7374942c1429b1184a19e4abb0d9cd641d3a805b
williamrolando88 <william.rolando88@hotmail.com>
2022-06-07 16:22:14 -0500

    Delete access to products#index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :lines do
     resources :brands, except:[:index] do
-      resources :products
+      resources :products, except:[:index]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
