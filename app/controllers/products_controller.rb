1a80f8be4fa4ae3a17a9cca53619eb26b6d46dac
williamrolando88 <william.rolando88@hotmail.com>
2022-06-07 14:28:37 -0500

    Fix destroy product redirection

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -49,7 +49,7 @@ class ProductsController < ApplicationController
     @product.destroy
 
     respond_to do |format|
-      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
+      format.html { redirect_to line_brand_path(@line, @brand), notice: "Product was successfully destroyed." }
       format.json { head :no_content }
     end
   end
