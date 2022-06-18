fdd1df2447c9759e34cd66eb35a6063da2a455c0
williamrolando88 <william.rolando88@hotmail.com>
2022-06-17 20:55:00 -0500

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  require 'securerandom'
+
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_one_attached :avatar
 end
