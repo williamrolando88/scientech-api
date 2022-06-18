d5220cbd980f29a018674c54425e436e2cf6ee83
williamrolando88 <william.rolando88@hotmail.com>
2022-06-17 21:21:45 -0500

    Add `User` controller methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,43 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
+  # GET /users/{username}
+  def show
+    @user = User.find(params[:id])
+    render json: @user, status: :ok
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessible_entity
+    end
+  end
+
+  # PUT /users/{username}
+  def update
+    return if @user.update(user_params)
+
+    render json: { errors: @user.errors.full_messages },
+           status: :unprocessible_entity
+  end
+
+  # DELETE /users/{username}
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :role, :email, :password, :avatar)
+  end
 end
