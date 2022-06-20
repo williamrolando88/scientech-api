92aa9c8a91b670ad62ec261f7ef57eb1bed1bbd7
williamrolando88 <william.rolando88@hotmail.com>
2022-06-20 18:34:18 -0500

    Add sign_up method

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,30 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_request
+  
+  # POST /auth/login
+  def authenticate
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      token = JsonWebToken.encode({ user_id: user.id, role: user.role })
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
+
+    # POST /auth/signup
+    def sign_up
+      user = User.create(user_params)
+      if user.save
+        render json: user, status: :created
+      else
+        render json: { errors: user.errors.full_messages },
+               status: :unprocessible_entity
+      end
+    end
+
+    private 
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :role, :avatar)
+    end
+  end
