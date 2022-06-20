38f6c4cc97ade05a33a17c6675f178deaa2c6bf1
williamrolando88 <william.rolando88@hotmail.com>
2022-06-20 16:39:20 -0500

    Fix `unauthorized` status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,13 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_request
+    unless request.headers['Authorization'].present?
+      render json: { error: 'Not Authorized' }, status: :unauthorized
+      return
+    end
+
     header = request.headers['Authorization']
-    header = header.split.last if header
+    header = header.split('.').last
     decoded = jwt_decode(header)
     @current_user = User.find(decoded[:user_id])
   end
