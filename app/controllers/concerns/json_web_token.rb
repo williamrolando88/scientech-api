be0c5c6a8f6e1995987becb0a6c07ce8e7363173
williamrolando88 <william.rolando88@hotmail.com>
2022-06-20 20:03:34 -0500

    Add HS256 encryption algorithm

--- app/controllers/concerns/json_web_token.rb
+++ app/controllers/concerns/json_web_token.rb
@@ -1,15 +1,15 @@
 require 'jwt'
 module JsonWebToken
   extend ActiveSupport::Concern
-  SECRET_KEY = Rails.application.credentials.secret_key_base
+  SECRET_KEY = Rails.application.credentials.jwt_key!
 
   def jwt_encode(payload, exp = Time.zone.now + 1.day)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, SECRET_KEY, 'HS256')
   end
 
   def jwt_decode(token)
-    decoded = JWT.decode(token, SECRET_KEY)[0]
+    decoded = JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })[0]
     HashWithIndifferentAccess.new decoded
   end
 end
