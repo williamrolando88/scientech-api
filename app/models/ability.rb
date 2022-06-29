8078b5a481e3994bbc2bd02c24616d05ad2ad786
williamrolando88 <william.rolando88@hotmail.com>
2022-06-29 11:39:53 -0500

    Update `User` abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,38 +4,26 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    # Basic authorization level
+    can :read, Brand
+    can :read, Line
+    can :read, Product
 
+    # Logged user authorization
     return unless user.present?
 
     can [:read, :update], User, id: user.id
+
+    # Admin authorization
     return unless user.role == 'admin'
 
     can :manage, User
+    cannot :manage, User, user.role == 'super_user'
+
+    # Super user authorization
+    return unless user.role == 'super_user'
+
+    can :manage, :all
+    cannot :delete, User, user.role == 'super_user'
   end
 end
