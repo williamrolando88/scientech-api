fa9c18e3b3f680e6c0ed5617f433f936b5ea3ebf
williamrolando88 <william.rolando88@hotmail.com>
2022-06-29 11:04:37 -0500

    Add seed to create super_user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+=begin 
+
+super_user:
+  email: it@scientech-ec.com
+  name: Super User
+  role: super_user
+  password: $CJ5lGi*^R^S@#qWRrOt
+
+
+=end
+
+User.create!(
+             {
+              name: Rails.application.credentials.dig(:super_user, :name),
+              email: Rails.application.credentials.dig(:super_user, :email),
+              role: Rails.application.credentials.dig(:super_user, :role),
+              password: Rails.application.credentials.dig(:super_user, :password)
+             }
+            )
